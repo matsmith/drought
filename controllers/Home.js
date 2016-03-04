8ef652e175c2edf1746ea3f3c6f93bd161400c7d
Mathew Smith <msmit205@its.jnj.com>
2016-03-04 14:12:38 -0500

    re-joining master and local

--- controllers/Home.js
+++ controllers/Home.js
@@ -7,41 +7,41 @@ module.exports = BaseController.extend({
 	run: function(req, res, next) {
 		var self = this;
 		this.content.htmlClass = 'front not-logged-in page-home no-sidebars fluid-width locale-en_us home-page-not-logged-in';
-		// Transform jQuery not-real promise into real promise
-		// Promise.all([
-		// 	self.service.presentation.getBy({ page: 'portal', type: 'navigation' }),
-		// 	self.service.companyConfig.getCompanyConfig(['ProductRenaming'])
-		// ]).then(function(serviceResults){
-		// 	self.getNav(serviceResults[0]);
-		// 	self.getContent(serviceResults[1]);
-		// }).then(undefined, function(err){
-		// 	var exception = JSON.parse(err.responseText);
-		// 	console.log('Promise Error Type: ', exception.error);
-		// 	console.log('Promise Error Description: ', exception.error_description);
-		// }).done(function(success, error){
+		Transform jQuery not-real promise into real promise
+		Promise.all([
+			self.service.presentation.getBy({ page: 'portal', type: 'navigation' }),
+			self.service.companyConfig.getCompanyConfig(['ProductRenaming'])
+		]).then(function(serviceResults){
+			self.getNav(serviceResults[0]);
+			self.getContent(serviceResults[1]);
+		}).then(undefined, function(err){
+			var exception = JSON.parse(err.responseText);
+			console.log('Promise Error Type: ', exception.error);
+			console.log('Promise Error Description: ', exception.error_description);
+		}).done(function(success, error){
 			self.content.programName = "Mat App";
 			self.content.nav = [];
 			self.content.subNav = [];
 		 	new View(res, 'home').render( self.content, { navigation: '_nav' } );
-		// });
+		});
 	},
 	getContent: function(data) {
-		// var self = this;
-		// this.content.programName = data.model.features.ProductRenaming.bingeLongName;
+		var self = this;
+		this.content.programName = data.model.features.ProductRenaming.bingeLongName;
 	},
 	getNav: function(data) {
-		// var self = this;
-		// var primaryNav = [], subNav = [];
-		// data.model.areas.forEach(function(area){
-		// 	area.blocks.forEach(function(block){
-		// 		if(block.hasOwnProperty('blocks')){
-		// 			subNav = block.blocks;
-		// 		} else {
-		// 			primaryNav.push(block);
-		// 		}
-		// 	});
-		// 	self.content.nav = primaryNav;
-		// 	self.content.subNav = subNav;
-		// });
+		var self = this;
+		var primaryNav = [], subNav = [];
+		data.model.areas.forEach(function(area){
+			area.blocks.forEach(function(block){
+				if(block.hasOwnProperty('blocks')){
+					subNav = block.blocks;
+				} else {
+					primaryNav.push(block);
+				}
+			});
+			self.content.nav = primaryNav;
+			self.content.subNav = subNav;
+		});
 	}
 });
